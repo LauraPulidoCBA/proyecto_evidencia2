267857f6513164ac0fdfa476c8943ac312dfe422
LauraPulidoCBA <laurapulido.cba.sena@gmail.com>
2026-09-02 22:22:46 -0500

    Corregir test_app.py para pipeline CI/CD Evidencia2

--- test_app.py
+++ test_app.py
@@ -1,7 +1,9 @@
-import requests
+import pytest
+from app import app
 
-def test_api_responde():
-    url = "https://api-evidencia2.duckdns.org"
-    response = requests.get(url)
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
     assert response.status_code == 200
-    assert "API Fase 1 funcionando!" in response.text
+    assert b"API Fase 1 funcionando! - actualizado" in response.data
+
